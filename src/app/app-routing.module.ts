a7b94748fff54b119acfbb3cccc79fa9cd54cba0
Paweł Rykała <pawel.rykala23@o2.pl>
2021-06-06 12:07:57 +0200

    new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountComponent } from './components/account/account.component';
+import { CartComponent } from './components/cart/cart.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
+import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { SearchComponent } from './components/search/search.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent }, 
+  { path: 'home', component: HomeComponent }, 
+  { path: 'cart', component: CartComponent }, 
+  { path: 'search', component: SearchComponent }, 
+  { path: 'messages', component: MessagesComponent }, 
+  { path: 'favorites', component: FavoritesComponent }, 
+  { path: 'account', component: AccountComponent }, 
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MdbModule } from 'mdb-angular-ui-kit';
+import { MessagesComponent } from './components/messages/messages.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
+import { SearchComponent } from './components/search/search.component';
+import { CartComponent } from './components/cart/cart.component';
+import { HomeComponent } from './components/home/home.component';
+import { AccountComponent } from './components/account/account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NavbarComponent,
+    MessagesComponent,
+    FavoritesComponent,
+    SearchComponent,
+    CartComponent,
+    HomeComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MdbModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
