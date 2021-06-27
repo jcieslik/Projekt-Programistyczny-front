4884cdc35eee9c80ed40983ac9f28be0610f5e76
Paweł Rykała <pawel.rykala23@o2.pl>
2021-06-27 20:10:04 +0200

    province service added

---
+++ src/app/services/province/province.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Province } from 'src/app/models/province';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProvinceService {
+
+  constructor(private http: HttpClient) { }
+
+  getProvinces(){
+    return this.http.get<Province[]>(`${environment.apiUrl}/api/Province/GetProvinces`);
+  }
+  
+}
